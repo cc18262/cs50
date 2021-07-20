fa2cb7dd88ffbf819e44afdf0341850819dadc83
cc18262 <cc18262@kumgaku.ac.jp>
2021-07-20 22:11:34 +0900

    model cant update

--- Django/airline/flights/urls.py
+++ Django/airline/flights/urls.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
+
+from . import views
+
+urlpatterns = [
+    #path("", views.index, name="index"),
+    #path('flights/', include("flights.urls")),
+]
